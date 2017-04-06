cd80e5af2d339fbfa6be657b10d0645afb3ed6b9
Iain Reid <iainreid382@gmail.com>
2017-04-06 18:31:11 +0100

    [chore] Added initial Scala example

---
+++ scala/src/main/scala/main.scala
@@ -0,0 +1,37 @@
+object KnightsTour {
+
+  sealed trait TourMethod {
+    def methodName: String
+
+    def exec: Unit
+  }
+
+  object TourMethod {
+    case object BruteForce extends TourMethod {
+      val methodName = "BruteForce"
+
+      def exec = {
+        ()
+      }
+    }
+
+    case object BreakWhenBlocked extends  TourMethod {
+      val methodName = "BreakWhenBlocked"
+
+      def exec = {
+        ()
+      }
+    }
+
+    def parse(input: String): TourMethod = input match {
+      case BruteForce.methodName => BruteForce
+      case BreakWhenBlocked.methodName => BreakWhenBlocked
+      case _ => throw new IllegalArgumentException(s"Unknown tour method: [$input]")
+    }
+  }
+
+  def main(args: Array[String]) {
+    TourMethod.parse(args(0)).exec
+  }
+
+}
